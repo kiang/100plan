371b9ddc10b04200b7b2eac7e666bfa0daac625d
kiang <kiange@gmail.com>
2022-11-24 11:48:16 +0800

    generate list

--- 2022/download.php
+++ 2022/download.php
@@ -2,6 +2,7 @@
 $page = file_get_contents('https://www.tainan.gov.tw/News_Photo_Content.aspx?n=36312&s=7924003');
 $needle = '<a   href="https://w3fs.tainan.gov.tw/001/Upload/1/relpic/';
 $pos = strpos($page, $needle);
+$listFh = fopen(__DIR__ . '/list.csv', 'w');
 while (false !== $pos) {
     $posEnd = strpos($page, 'data-title=', $pos);
     $parts = explode('"', substr($page, $pos, $posEnd - $pos));
@@ -9,6 +10,8 @@ while (false !== $pos) {
     if (substr($parts[3], -3) !== 'jpg') {
         $parts[3] = $parts[3] . '.jpg';
     }
+    $listLine = substr($parts[3], 0, -4);
+    fputcsv($listFh, [substr($listLine, 0, 4), substr($listLine, 4)]);
     $targetFile = __DIR__ . '/img/' . str_replace(' ', '_', $parts[3]);
     if (!file_exists($targetFile)) {
         $p = pathinfo($parts[1]);
